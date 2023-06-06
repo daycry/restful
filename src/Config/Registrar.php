0d5129de797acddb86ec080de634405841ccb047
daycry <7590335+daycry@users.noreply.github.com>
2023-06-06 18:25:37 +0200

    Fix cors

--- src/Config/Registrar.php
+++ src/Config/Registrar.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Daycry\RestFul\Config;
 
 use Daycry\RestFul\Filters\AccessFilter;
-use Daycry\RestFul\Filters\CorsFilter;
 
 class Registrar
 {
--- src/Config/Routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace Daycry\RestFul\Config;
-
-/** @var \CodeIgniter\Router\RouteCollection $routes */
-$routes->options('(:any)', '', ['filter' => 'cors']);
